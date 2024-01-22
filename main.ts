cff4621948a2e529e85ad86d9e7d670895195b64
AnZaha <156139867+AnZaha@users.noreply.github.com>
2024-01-22 16:09:56 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-function Velocity_Increase_Points (Level: number) {
-    Fish_Food = sprites.create(img`
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Fish_Food = sprites.createProjectileFromSprite(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
@@ -16,13 +16,34 @@ function Velocity_Increase_Points (Level: number) {
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
-        `, SpriteKind.Player)
-    Fish_Food.setPosition(155, randint(20, 85))
-    Fish_Food.setVelocity(100, 30)
+        `, Off_right_projectile_spawner, 50, 50)
+})
+function Velocity_Increase_Points (Level: number) {
+	
 }
 let Fish_Food: Sprite = null
+let Off_right_projectile_spawner: Sprite = null
 let Level = 0
 let Fish = sprites.create(assets.image`myImage`, SpriteKind.Player)
+Off_right_projectile_spawner = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 3 3 3 3 3 . . . . . 
+    . . . . . . 3 3 3 3 3 . . . . . 
+    . . . . . . 3 3 3 3 3 . . . . . 
+    . . . . . . 3 3 3 3 3 . . . . . 
+    . . . . . . 3 3 3 3 3 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+tiles.placeOnTile(Off_right_projectile_spawner, tiles.getTileLocation(11, 240))
 Fish.setVelocity(30, 0)
 controller.moveSprite(Fish, 40, 40)
 scene.setBackgroundImage(assets.image`background water`)
@@ -31,4 +52,6 @@ Fish.ax = 30
 tiles.setCurrentTilemap(tilemap`level`)
 scene.cameraFollowSprite(Fish)
 tiles.placeOnTile(Fish, tiles.getTileLocation(0, 252))
-Velocity_Increase_Points(Level)
+forever(function () {
+	
+})
